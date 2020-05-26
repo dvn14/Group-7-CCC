3d855112b5bbb8b5d2aae112f3e1dab93c7c8d61
Devin Nanayakkara <nanayakkara.dv@gmail.com>
2020-05-26 23:04:58 +1000

    finilised repository

--- CouchDB/create_views.py
+++ CouchDB/create_views.py
@@ -2,7 +2,7 @@ import json
 
 from couchdb.client import Server
 
-server = Server('http://admin:admin@172.26.129.71:5984')
+server = Server('http://admin:admin@localhost:5984')
 db = server['my_db4']
 
 with open('mapper.json', 'r') as f:
---
+++ twitter-harvester/params.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 14:26:24 2020
+
+@author: devin
+CCC2020 - Group 7
+"""
+
+NSW_GEO = "geo_files/nsw_geo.json"
+AUS_GEO = "geo_files/aus_geo.json"
+
+STREAM_OUTPUT = "tweets_backup/stream.json"
+SEARCH_OUTPUT = "tweets_backup/search.json"
+
+"""
+For STREAM API
+"""
+
+CONSUMER_KEY = "Ok4i5Q2xbei7zbalAT8CNtIyP"
+CONSUMER_SECRET = "8run02ATBTC5U5uNYfmGDHjLtn4h4WGEPIu3BAqjjt0k8ocBQX"
+ACCESS_TOKEN = "1251884711271477248-U2x2FW6YOwBXUkGnAZJYcK5KeF0sxh"
+ACCESS_TOKEN_SECRET = "dHfdzos7zWVtTfRjrXDawvJM9xNUitRMTMt40tm6s1M0n"
+
+AUS_GEO_RANGE = [112,-44,155,-10]
+
+"""
+For SEARCH API
+"""
+
+SYD_GEO_RANGE = '-33.868800, 151.209300,100mi'
+
+CONSUMER_KEY_SET = ["okn6Jdvfi0Ac8ar2Scczxoct1",
+                    "PXja7gxzJkOxaKjGA9AXG8V25",
+                    "DbpI8TlIe7l2daj4X1401XhAq"]
+CONSUMER_SECRET_SET = ["Y8RqzDjR215IIzfDsnCFW2n2NncsKN6JIYg33nxFAFfr4eDQSo",
+                       "h95Q9x9c5LBoZWPHupyQ8Ul5XlsDTIfPbsGJ5lqyeOfzCjlEZp",
+                       "acK2kR75H3MQoUG7zABBnQVVRX0UAA0RmBLhqtKvI5NBgEPVoW"]
+ACCESS_TOKEN_SET = ["1261171801565958145-LMOHEU4MLYZAlcTCKbtaITl26wUSxS",
+                    "91487683-7I9nUp38O2gCuBUjwjau4y1xUhAKb53AdhngLfBdo",
+                    "1262048605713477632-bmA8JH1eeXKrC4RcTbHa0loyjWJ0WT"]
+ACCESS_TOKEN_SECRET_SET = ["oqXzKcMETOriCnHtS0bmBYkbtUVALKw1Bv15Mjj6lRpsv",
+                           "Lo8E5Sbl3ZB4Th29DG6g3F8DgPjrqX7LNAyoGvXYQAkve",
+                           "uv4laBARug4Jbl6z592IUsH0QON05WwGl4z5uOltemJnd"]
+
+
+
